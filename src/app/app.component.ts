61d9bb4d922d3f2bf8a7569ce49518fb881e0e27
taras-gr <tar.grytsenko@gmail.com>
2019-10-06 20:16:11 +0300

    beta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,13 @@ export class AppComponent implements AfterViewInit {
   trigrams: boolean = true;
   lastLetters: boolean = true;
 
-  valueOfSelect = '';
+  lettersOrder = 'frq';
+  bigramsOrder = 'frq';
+  trigramsOrder = 'frq';
+  lastLettersOrder = 'frq';
 
-  text ='';
-  
-  
-  canvas;
-  ctx;
-  chart;
+  text ='';  
+  maxCount : any = 100;
 
   charts = [];
   chartNames = ["Letters", "Bigrams", "Trigrams", "LastLetters"];
@@ -32,15 +31,19 @@ export class AppComponent implements AfterViewInit {
   ngAfterViewInit() {
     for(var i = 0; i < this.chartNames.length; i++)
     {
+      if (this.charts[i]) {
+        this.charts[i].destroy();
+      }
+
         this.canvases[i] = <HTMLCanvasElement>document.getElementById(this.chartNames[i]);
         this.ctxs[i] = this.canvases[i].getContext('2d');
 
         this.charts[i] = new Chart(this.ctxs[i], {
-          type: 'horizontalBar',
+          type: 'bar',
           data: {
               labels: [],
               datasets: [{
-                  label: '# of occurrences',
+                  label: 'Occurrence of ' + this.chartNames[i],
                   data: [],
                   backgroundColor: [
                   ],
@@ -50,29 +53,59 @@ export class AppComponent implements AfterViewInit {
               }]
           },
           options: {
-              responsive: false,
+              responsive: true,
               scales: {
                   yAxes: [{
                       ticks: {
-                          beginAtZero: true
+                          beginAtZero: true,
                       }
-                  }]
+                  }],
+                  xAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                    }
+                }]
               }
           }
       });
     }
  }
 
+ onChange(newValue) {
+    this.onClickMe();
+ }
+ 
   onClickMe(){
     this.ngAfterViewInit();
-    console.log(this.valueOfSelect);
 
     for (var i = 0; i < this.charts.length; i++)
     {
       var combinations = this.getCombinations(this.text, i + 1);
-      this.fillChartData(combinations, this.charts[i]);
-      this.fillChartColors(combinations, this.charts[i]);
+      if (i == 0)
+      {
+        this.fillChartData(combinations, this.charts[i], this.lettersOrder);
+        this.fillChartColors(combinations, this.charts[i]);
       this.charts[i].update();
+      }
+      if (i == 1)
+      {
+        this.fillChartData(combinations, this.charts[i], this.bigramsOrder);
+        this.fillChartColors(combinations, this.charts[i]);
+      this.charts[i].update();
+      }
+      if (i == 2)
+      {
+        this.fillChartData(combinations, this.charts[i], this.trigramsOrder);
+        this.fillChartColors(combinations, this.charts[i]);
+      this.charts[i].update();
+      }
+      if (i == 3)
+      {
+        this.fillChartData(this.getLastLetters(this.text), this.charts[i], this.lastLettersOrder);
+
+        this.fillChartColors(this.getLastLetters(this.text), this.charts[i]);
+      this.charts[i].update();
+      }
     }
     
   }
@@ -87,7 +120,7 @@ export class AppComponent implements AfterViewInit {
       var r = this.getRandomInt(256);
       var g = this.getRandomInt(256);
       var b = this.getRandomInt(256);
-      var a = 0.2;
+      var a = 1.0;
       var aOne = 1.0;
 
       backgroundColors[i] = ('rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')');
@@ -98,34 +131,55 @@ export class AppComponent implements AfterViewInit {
     chart.data.datasets[0].borderColor = borderColors;
   }
 
-  fillChartData(combinations: KeyedCollection<number>, chart)
+  fillChartData(combinations: KeyedCollection<number>, chart, order: string)
   {
-    var klychi: string[] = []; 
-    
-    var valuesUnsorted = Object.values(combinations.items);  
-    var keys = Object.keys(combinations.items);  
-    var values = Object.values(combinations.items); 
-    
-    values.sort(function(a, b){return b-a});
-
-    for (var i = 0; i < values.length; i++)
-    {
-      for(var j = 0; j < valuesUnsorted.length; j++)
+      if (order == "frq")
       {
-        if(values[i] == valuesUnsorted[j])
+      var klychi: string[] = []; 
+      
+      var valuesUnsorted = Object.values(combinations.items);  
+      var keys = Object.keys(combinations.items);  
+      var values = Object.values(combinations.items); 
+      
+      values.sort(function(a, b){return b-a});
+
+      for (var i = 0; i < values.length; i++)
+      {
+        for(var j = 0; j < valuesUnsorted.length; j++)
         {
-          if(!klychi.includes(keys[j]))
+          if(values[i] == valuesUnsorted[j])
           {
-            klychi[i] = keys[j];
+            if(!klychi.includes(keys[j]))
+            {
+              klychi[i] = keys[j];
+            }
           }
+        }
+      }
+
+      for (var i = 0; i < this.maxCount; i++)
+      {
+        if (klychi[i] != undefined)
+        {
+          chart.data.datasets[0].data[i] = values[i];
+          chart.data.labels[i] = klychi[i];
         }
       }
     }
 
-    for (var i = 0; i < 100; i++)
-    {
-      chart.data.datasets[0].data[i] = values[i];
-      chart.data.labels[i] = klychi[i];
+    else{
+      var keys11 = Object.keys(combinations.items);
+      
+      keys11.sort();
+
+      for (var i = 0; i < this.maxCount; i++)
+      {
+        if(combinations.Item(keys11[i]) != undefined){
+
+        chart.data.datasets[0].data[i] = combinations.Item(keys11[i]);
+        chart.data.labels[i] = keys11[i];
+        }
+      }
     }
   }
 
@@ -149,6 +203,46 @@ export class AppComponent implements AfterViewInit {
           combinations.Add(tempString, 1);
         }
 
+      }
+    }
+
+    return combinations;
+  }
+
+  getLastLetters(text: string) : KeyedCollection<number>{
+    var str = text;
+
+    var combinations = new KeyedCollection<number>();
+
+    for(var i = 0; i < str.length - 1; i++)
+    {
+      if(combinations.ContainsKey(str[i]) && !(str[i] == "." || str[i] == ")" || str[i] == "," || str[i] == "!"
+       || str[i] == "?" || str[i] == "\"" || str[i] == ":" || str[i] == ";" || str[i] == "-" || str[i] == "]" || str[i] == "-") && (
+        str[i + 1] == " "
+        || str[i + 1] == "."
+        || str[i + 1] == ","
+        || str[i + 1] == ")"
+        || str[i + 1] == "\""
+        || str[i + 1] == "!"
+        || str[i + 1] == "?"))
+      {
+        combinations.items[str[i]]++;
+      }
+
+      else{
+        if (!(str[i] == "." || str[i] == ")" || str[i] == "," || str[i] == "!" || 
+        str[i] == "?" || str[i] == "\"" || str[i] == ":" || str[i] == ";" || str[i] == "-" || str[i] == "]" || str[i] == "-") && (
+          str[i + 1] == " "
+          || str[i + 1] == "."
+          || str[i + 1] == ","
+          || str[i + 1] == ")"
+          || str[i + 1] == "\""
+          || str[i + 1] == "!"
+          || str[i + 1] == "?"))
+          {
+           combinations.Add(str[i], 1);
+   
+          }
       }
     }
 
